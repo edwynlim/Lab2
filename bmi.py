6bb1c64bd6d119fe8ba928697c6b65b0bb242ae0
ed <edwynlim.21@ichat.sp.edu.sg>
2022-10-25 11:18:31 +0800

    bmi commit

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    BMI = weight/(height*height)
+    print("BMI = "+str(BMI))
+
+    if BMI < 18.5:
+        print("User is underweight")
+    elif 18.5 <= BMI <= 25.0:
+        print("User has normal weight")
+    else:
+        print("User is overweight")
+
+calculate_bmi(weight=57, height=1.73)
